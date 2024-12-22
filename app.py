6aa35a6d4820542bc635b29953e6893d916de197
LuizGabrielSS <luizgabriel11022001@gmail.com>
2024-12-22 10:07:10 -0300

    feat: adicionando função principal

---
+++ app.py
@@ -0,0 +1,16 @@
+from huggingface_hub import login
+
+from components.logger import logger
+from components.timer_decorator import timer
+from components.get_secret import get_secret
+
+from src.report_email.init import report_email
+
+@timer('Função principal')
+def main():
+
+    result = report_email()
+
+    print(result)
+
+main()
